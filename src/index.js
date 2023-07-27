e21ff0ba9ea78eb88df78be145bf417759415e45
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-07-27 15:34:23 -0400

    Controladores y rutas añadidas

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import envs from './configs/environments.js';
 import connect from './configs/mongo.js';
+import AlbumRoutes from './routes/album.routes.js';
+import ArtistRoutes from './routes/artist.routes.js';
+import SongRoutes from './routes/song.routes.js';
+import UserRoutes from './routes/user.routes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/album", AlbumRoutes);
+app.use("/artist", ArtistRoutes);
+app.use("/song", SongRoutes);
+app.use("/user", UserRoutes);
 
 console.log('Conectando a la base de datos...');
 connect()
---
+++ src/routes/user.routes.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import {  } from '../controllers/user.controller.js';
+// import { auth } from '../middlewares/auth.js';
+// import multer from 'multer';
+
+// // Configuración de subida
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "./uploads/avatars/")
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, "avatar-" + Date.now() + "-" + file.originalname)
+//     }
+// });
+
+// const uploads = multer({ storage });
+
+const router = Router();
+
+// router.post('/register', register);
+
+// router.post('/login', login);
+
+// router.get('/profile/:userId', auth , profile);
+
+// router.get('/list/:page?', auth , list);
+
+// router.put('/update', auth , update);
+
+// router.post('/upload', [auth, uploads.single("file0")], upload);
+
+// router.get('/avatar/:file', showAvatar);
+
+// router.get('/counters/:userId', auth , counters);
+
+export default router;
