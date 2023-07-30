73bc9b01711a70177667a234d68b6adcadc61e0f
DanRuizK57 <d.ruiz03@ufromail.cl>
2023-07-29 20:17:48 -0400

    Obtener lista de álbums de un artista

--- src/controllers/album.controller.js
+++ src/controllers/album.controller.js
@@ -1,55 +1,78 @@
 import AlbumModel from "../models/album.model.js";
 
 async function save(req, res) {
+  try {
+    // Obtener datos de la petición
+    let params = req.body;
 
-    try {
-        // Obtener datos de la petición
-        let params = req.body;
+    // Crear obj album
+    let album = new AlbumModel(params);
 
-        // Crear obj album
-        let album = new AlbumModel(params);
+    // Guardar en la BBDD
+    await album.save();
 
-        // Guardar en la BBDD
-        await album.save();
-
-        return res.status(200).send({
-            status: "success",
-            album
-          });
-
-
-    } catch (err) {
-        return res
-          .status(500)
-          .send({ error: "Ha ocurrido un error en la base de datos" });
-      }
-
+    return res.status(200).send({
+      status: "success",
+      album,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ error: "Ha ocurrido un error en la base de datos" });
+  }
 }
 
 async function getAlbum(req, res) {
-    try {
-      // Obtener ID del album
-      const albumId = req.params.albumId;
-  
-      // Buscar en BBDD
-      let album = await AlbumModel.findById(albumId).populate("artist"); // Para obtener el obj completo de album
-  
-      if (!album) {
-        return res.status(404).send({
-          status: "error",
-          message: "Álbum no encontrado",
-        });
-      }
-  
-      return res.status(200).send({
-        status: "success",
-        album
-      });
-    } catch (err) {
-      return res
-        .status(500)
-        .send({ error: "Ha ocurrido un error en la base de datos" });
-    }
-  }
+  try {
+    // Obtener ID del album
+    const albumId = req.params.albumId;
 
-export { save, getAlbum };
+    // Buscar en BBDD
+    let album = await AlbumModel.findById(albumId).populate("artist"); // Para obtener el obj completo de album
+
+    if (!album) {
+      return res.status(404).send({
+        status: "error",
+        message: "Álbum no encontrado",
+      });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      album,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ error: "Ha ocurrido un error en la base de datos" });
+  }
+}
+
+async function list(req, res) {
+  try {
+
+    // Obtener ID del artista
+    const artistId = req.params.artistId;
+
+    const albums = await AlbumModel.find({ artist: artistId })
+      .populate("artist");
+
+    if (!albums) {
+      return res.status(404).send({
+        status: "error",
+        message: "No se encontraron álbums de este artista",
+      });
+    }
+
+    return res.status(200).send({
+      status: "success",
+      albums,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ error: "Ha ocurrido un error en la base de datos" });
+  }
+}
+
+export { save, getAlbum, list };
--- src/routes/album.routes.js
+++ src/routes/album.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { save, getAlbum } from '../controllers/album.controller.js';
+import { save, getAlbum, list } from '../controllers/album.controller.js';
 import { auth } from '../middlewares/auth.js';
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.post('/save', auth, save);
 
 router.get('/get/:albumId', auth, getAlbum);
+
+router.get('/list/:artistId', auth, list);
 
 export default router;
